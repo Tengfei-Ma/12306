2f18b78004a49801e84c7c4377bfd11a0422347b
mtf <m1501440483@email.swu.edu.cn>
2024-04-28 20:51:54 +0800

    开发持久层组件

---
+++ frameworks/database/src/main/java/org/mtf/index12306/framework/starter/database/handler/CustomIdGenerator.java
@@ -0,0 +1,15 @@
+package org.mtf.index12306.framework.starter.database.handler;
+
+import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
+import org.mtf.index12306.framework.starter.distributedid.toolkit.SnowflakeIdUtil;
+
+/**
+ * 自定义雪花算法生成器
+ */
+public class CustomIdGenerator implements IdentifierGenerator {
+
+    @Override
+    public Number nextId(Object entity) {
+        return SnowflakeIdUtil.nextId();
+    }
+}
