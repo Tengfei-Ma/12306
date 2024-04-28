6f765b5ee81d56baa9de5713be1ce3724cd36b43
mtf <m1501440483@email.swu.edu.cn>
2024-04-28 18:45:11 +0800

    开发公共组件

---
+++ frameworks/common/src/main/java/org/mtf/index12306/framework/starter/common/toolkit/EnvironmentUtil.java
@@ -0,0 +1,40 @@
+package org.mtf.index12306.framework.starter.common.toolkit;
+
+import org.mtf.index12306.framework.starter.bases.ApplicationContextHolder;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 环境工具类
+ */
+public class EnvironmentUtil {
+
+    private static List<String> ENVIRONMENT_LIST = new ArrayList<>();
+
+    static {
+        ENVIRONMENT_LIST.add("dev");
+        ENVIRONMENT_LIST.add("test");
+    }
+
+    /**
+     * 判断当前是否为开发环境
+     *
+     */
+    public static boolean isDevEnvironment() {
+        ConfigurableEnvironment configurableEnvironment = ApplicationContextHolder.getBean(ConfigurableEnvironment.class);
+        String propertyActive = configurableEnvironment.getProperty("spring.profiles.active", "dev");
+        return ENVIRONMENT_LIST.stream().filter(each -> propertyActive.contains(each)).findFirst().isPresent();
+    }
+
+    /**
+     * 判断当前是否为正式环境
+     *
+     */
+    public static boolean isProdEnvironment() {
+        ConfigurableEnvironment configurableEnvironment = ApplicationContextHolder.getBean(ConfigurableEnvironment.class);
+        String propertyActive = configurableEnvironment.getProperty("spring.profiles.active", "dev");
+        return !ENVIRONMENT_LIST.stream().filter(each -> propertyActive.contains(each)).findFirst().isPresent();
+    }
+}
