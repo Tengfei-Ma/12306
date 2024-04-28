2e051a9a13b0546ddf709a0bc5e9710308bbcc96
mtf <m1501440483@email.swu.edu.cn>
2024-04-28 18:47:00 +0800

    开发缓存组件

---
+++ frameworks/cache/src/main/java/org/mtf/index12306/framework/starter/cache/core/CacheLoader.java
@@ -0,0 +1,13 @@
+package org.mtf.index12306.framework.starter.cache.core;
+
+/**
+ * 缓存加载器
+ */
+@FunctionalInterface
+public interface CacheLoader<T> {
+
+    /**
+     * 加载缓存
+     */
+    T load();
+}
