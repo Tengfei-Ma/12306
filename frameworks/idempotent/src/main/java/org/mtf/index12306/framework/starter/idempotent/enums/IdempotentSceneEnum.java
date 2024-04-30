322dbadc295e07cb3e1fea556abc1d4578baa2ae
mtf <m1501440483@email.swu.edu.cn>
2024-04-30 21:20:05 +0800

    修改注释

--- frameworks/idempotent/src/main/java/org/mtf/index12306/framework/starter/idempotent/enums/IdempotentSceneEnum.java
+++ frameworks/idempotent/src/main/java/org/mtf/index12306/framework/starter/idempotent/enums/IdempotentSceneEnum.java
@@ -6,12 +6,12 @@ package org.mtf.index12306.framework.starter.idempotent.enums;
 public enum IdempotentSceneEnum {
     
     /**
-     * 基于 RestAPI 场景验证
+     * 基于 RestAPI 场景验证，防止重复提交
      */
     RESTAPI,
     
     /**
-     * 基于 MQ 场景验证
+     * 基于 MQ 场景验证，防止消息重复消费
      */
     MQ
 }
