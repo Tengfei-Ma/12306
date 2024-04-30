c9ed1a83872f482249565b9ac12480e62c0786bf
mtf <m1501440483@email.swu.edu.cn>
2024-04-30 16:09:08 +0800

    继承BaseDO

--- services/user-service/src/main/java/org/mtf/index12306/biz/userservice/dao/entity/UserReuseDO.java
+++ services/user-service/src/main/java/org/mtf/index12306/biz/userservice/dao/entity/UserReuseDO.java
@@ -2,13 +2,14 @@ package org.mtf.index12306.biz.userservice.dao.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
+import org.mtf.index12306.framework.starter.database.base.BaseDO;
 
 /**
  * 用户名复用实体
  */
 @Data
 @TableName("t_user_reuse")
-public class UserReuseDO {
+public class UserReuseDO extends BaseDO {
     /**
      * 用户名
      */
