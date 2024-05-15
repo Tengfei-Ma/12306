ef77fbf88003d95817532db82b04b1c4ed7d7ea1
mtf <m1501440483@email.swu.edu.cn>
2024-05-15 14:06:21 +0800

    重命名订单远程调用接口

--- services/pay-service/src/main/java/org/mtf/index12306/biz/payservice/remote/OrderRemoteService.java
+++ services/pay-service/src/main/java/org/mtf/index12306/biz/payservice/remote/OrderRemoteService.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * 车票订单远程服务调用
  */
 @FeignClient(value = "index12306-order${unique-name:}-service", url = "${aggregation.remote-url:}")
-public interface TicketOrderRemoteService {
+public interface OrderRemoteService {
 
     /**
      * 跟据订单号查询车票订单
