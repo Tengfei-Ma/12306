efebc92ddfb32f6d65e97456e61d71f9d8752372
mtf <m1501440483@email.swu.edu.cn>
2024-05-30 16:35:24 +0800

    修改注释

--- frameworks/idempotent/src/main/java/org/mtf/index12306/framework/starter/idempotent/config/IdempotentProperties.java
+++ frameworks/idempotent/src/main/java/org/mtf/index12306/framework/starter/idempotent/config/IdempotentProperties.java
@@ -8,13 +8,12 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * 幂等属性配置
- * 公众号：马丁玩编程，回复：加群，添加马哥微信（备注：12306）获取项目资料
  */
 @Data
 @ConfigurationProperties(prefix = IdempotentProperties.PREFIX)
 public class IdempotentProperties {
 
-    public static final String PREFIX = "congomall.idempotent.token";
+    public static final String PREFIX = "index12306.idempotent.token";
 
     /**
      * Token 幂等 Key 前缀
@@ -24,7 +23,7 @@ public class IdempotentProperties {
     /**
      * Token 申请后过期时间
      * 单位默认毫秒 {@link TimeUnit#MILLISECONDS}
-     * 随着分布式缓存过期时间单位 {@link RedisDistributedProperties#valueTimeUnit} 而变化
+     * 随着分布式缓存过期时间单位 {@link RedisDistributedProperties} 的 valueTimeUnit 而变化
      */
     private Long timeout;
 }
